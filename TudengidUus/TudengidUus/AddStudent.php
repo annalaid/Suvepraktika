2e8410db3cbc38b1111618dca7acfa908c5e4727
kaarelel <70900290+kaarelel@users.noreply.github.com>
2021-06-09 10:30:12 +0300

    Paar uuendust

---
+++ TudengidUus/TudengidUus/AddStudent.php
@@ -0,0 +1,115 @@
+<?php
+
+  require("fnc_AddStudent.php");
+  require("config.php");
+
+	$inputerror = "";
+    $firstname = "";
+    $lastname = "";
+    $email = "";
+    $personal_email = "";
+	$studentnotice = null;
+	$uliopilaskood = null;
+ 
+	$uliopilaskooderror = null;
+	$firstnameerror = null;
+	$lastnameerror = null;
+    $personal_emailerror = "";
+    $emailerror = "";
+    
+
+	if(isset($_POST["personsubmit"])) {
+		if(!empty($_POST["firstnameinput"])) {
+			$firstname .= ($_POST["firstnameinput"]);
+		} else {
+			$firstnameerror = "Palun sisesta eesnimi!";
+			
+		}
+		if(!empty($_POST["lastnameinput"])) {
+			$lastname .= ($_POST["lastnameinput"]);
+		} else {
+			$lastnameerror = "Palun sisesta perekonnanimi!";
+			
+		}
+
+		if(!empty($_POST["emailinput"])){
+			$email .= ($_POST["emailinput"]);
+		} else {
+			  $emailerror = "Palun sisesta kooliemail!";
+		  }
+
+		  if(!empty($_POST["personal_emailinput"])){
+			$personal_email .= ($_POST["personal_emailinput"]);
+		} else {
+			  $personal_emailerror = "Palun sisesta personaalne email!";
+		  }
+
+		  if(!empty($_POST["uliopilaskoodinput"])){
+			  $uliopilaskood .= ($_POST["uliopilaskoodinput"]);
+		  } else {
+			  $uliopilaskooderror = "Sisesta üliõpilaskood";
+		  }
+		
+		if(empty($firstnameerror) and empty($lastnameerror) and empty($emailerror) and empty($personal_emailerror) and empty($uliopilaskooderror)) {
+			$studentnotice = addstudent($firstname, $lastname, $email, $personal_email, $uliopilaskood);
+		}
+		echo "valmis";
+	}
+	
+	
+	
+?>
+
+<head>
+<link rel="stylesheet" href="style.css">
+<title>Lisa tudeng</title>
+
+</head>
+ 
+<body>
+<h3>Lisa tudeng</h3>
+<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
+ 
+<table align="center" cellpadding = "20">
+ 
+<tr>
+<td>Eesnimi:</td>
+<td><input name="firstnameinput" id="firstnameinput" type="text" value="<?php echo $firstname; ?>"><span><?php echo $firstnameerror; ?></span>
+</td>
+</tr>
+ 
+<tr>
+<td>Perekonna nimi:</td>
+<td><input name="lastnameinput" id="lastnameinput" type="text" value="<?php echo $lastname; ?>"><span><?php echo $lastnameerror; ?></span>
+</td>
+</tr>
+
+<tr>
+<td>Kooli email: </td>
+<td><input name="emailinput" id="emailinput" type="email" value="<?php echo $email; ?>"><span><?php echo $emailerror; ?></span></td>
+</tr>
+
+<tr>
+<td>Isiklik email: </td>
+<td><input name="personal_emailinput" id="personal_emailinput" type="email" value="<?php echo $personal_email; ?>"><span><?php echo $personal_emailerror; ?></span></td>
+</tr>
+
+<tr>
+<td>Lisa üliõpilaskood: </td>
+<td><input name="uliopilaskoodinput" id="uliopilaskoodinput" type="text" value="<?php echo $uliopilaskood; ?>"><span><?php echo $uliopilaskooderror; ?></span></td>
+</tr>
+
+<tr>
+<td colspan="2" align="center">
+<input name="personsubmit" type="submit" value="Salvesta">
+</td>
+</tr>
+</table>
+ 
+<span><?php echo $firstnameerror. " " .$lastnameerror. " ". $emailerror. " " .$personal_emailerror. " " .$uliopilaskooderror; ?></span>
+<p><?php echo $studentnotice; ?></p>
+</form>
+ 
+</body>
+</html>
---
+++ TudengidUus/TudengidUus/fnc_AddStudent.php
@@ -0,0 +1,39 @@
+<?php
+//funktsioonid, mida või kuidas kasutada
+  $database = "if20_pille_suvepraktika";
+
+    
+    function addstudent($firstname, $lastname, $email, $personal_email, $uliopilaskood){
+      echo "algus";
+      $conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
+      $stmt = $conn->prepare("SELECT firstname FROM student WHERE (firstname = ? AND lastname = ?)");
+      //echo $conn->error;
+      $stmt->bind_param("ss", $firstname, $lastname);
+      $stmt->bind_result($firstnamefromdb);
+      if($stmt->execute()) {
+        if($stmt->fetch()) {
+          $notice = "See tudeng on juba andmebaasis!";
+        } else {
+          $stmt->close();
+          $stmt = $conn->prepare("INSERT INTO student (firstname, lastname, email, personal_email, uliopilaskood) VALUES (?, ?, ?, ?, ?)");
+          echo $conn->error;
+          $stmt->bind_param("sssss", $firstname, $lastname, $email, $personal_email, $uliopilaskood);
+          if($stmt->execute()) {
+            $notice = "Tudengi info salvestatud!";
+          } else {
+            $notice = $stmt->error;
+          }
+        }
+      } else {
+        $notice = $stmt->error;
+      }
+      $stmt->close();
+      $conn->close();
+      echo "ots";
+      return $notice;
+    }
+	
+
+  
+
+?>
