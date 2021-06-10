1a5de3ec76c585c8f212790017ca4b17dd03d11e
Rico Angel Nurmsalu <70900261+RicoNurmsalu@users.noreply.github.com>
2021-06-10 11:56:35 +0300

    Praktika 4.päev
    Sildi leht valmis?

---
+++ Praktika/Proov.php
@@ -0,0 +1,77 @@
+<?php
+
+require("fnc_addTag.php");
+require("config.php");
+require_once "db.php";
+
+$sql = "SELECT tag_id, tag_name FROM tag";
+$result = mysqli_query($conn, $sql);
+
+  $tag_name = "";
+  $tagnotice = null;
+  $tag_nameerror = null;
+
+  if(isset($_POST["personsubmit"])) {
+		if(!empty($_POST["tag_nameinput"])) {
+			$tag_name .= ($_POST["tag_nameinput"]);
+		}
+    else {
+			$tag_nameerror = "Palun sisesta silt";
+      }
+    if(empty($tag_nameerror)){
+      $tagnotice = addTag($tag_name);
+      
+    }
+			
+	}
+?>
+
+
+<html>
+<head>
+<title>Sildid</title>
+<link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+
+ 
+<body>
+<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
+<td>Lisa silt: </td>
+<tr><td><input name="tag_nameinput" id="tag_nameinput" type="text" value="<?php echo $tag_name; ?>"><span><?php echo $tag_nameerror; ?></span></td></tr>
+<input name="personsubmit" type="submit" value="Salvesta">
+<span><?php echo $tag_nameerror ?></span>
+<p><?php echo $tagnotice; ?></p>
+
+
+<form name="Label" method="post" action="">
+<div style="width:500px;">
+<table border="0" cellpadding="10" cellspacing="1" width="500" class="tblListForm">
+<tr class="listheader">
+<td>Kuvatud sildid</td>
+<td>Kustuta</td>
+
+<?php
+$i=0;
+while($row = mysqli_fetch_array($result)) {
+if($i%2==0)
+$classname="evenRow";
+else
+$classname="oddRow";
+?>
+
+<tr class="<?php if(isset($classname)) echo $classname;?>">
+<td><?php echo $row["tag_name"]; ?></td>
+<td><a href="DeleteTag.php?tag_id=<?php echo $row["tag_id"]; ?>"  class="link"><img alt='Delete' title='Kustuta' src='images/delete.png' width='15px' height='15px'hspace='10' /></a></td>
+</tr>
+
+<?php
+$i++;
+}
+?>
+
+</table>
+</form>
+</div>
+</body>
+</html>
---
+++ Praktika/fnc_addTag.php
@@ -0,0 +1,40 @@
+<?php
+    $database = "if20_pille_suvepraktika";
+
+        function addTag($tag_name){
+            $conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
+            $stmt = $conn->prepare("SELECT tag_name FROM tag WHERE (tag_name = ?)");
+            $stmt->bind_param("s", $tag_name);
+            $stmt->bind_result($tag_namefromdb);      
+            if($stmt->execute()) {
+                if($stmt->fetch()) {
+                  $notice = "See silt on juba olemas";
+                } else {
+                  $stmt->close();
+                  $stmt = $conn->prepare("INSERT INTO tag (tag_name) VALUES (?)");
+                  echo $conn->error;
+                  $stmt->bind_param("s", $tag_name);
+                  if($stmt->execute()) {
+                    $notice = "Sildi info salvestatud!";
+                  } else {
+                    $notice = $stmt->error;
+                  }
+                }
+              } else {
+                $notice = $stmt->error;
+              }
+              $stmt->close();
+              $conn->close();
+              echo "ots";
+              //return $notice;
+            }
+         
+       
+          
+
+
+
+
+
+
+?>
