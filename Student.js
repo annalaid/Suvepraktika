36059e3d0d5ac1a06cd7a60b1c5678ba5a4d1eac
Pille Allvee <70939481+pilleae@users.noreply.github.com>
2021-06-16 12:02:38 +0300

    Delete Student.js

--- Student.js
+++
@@ -1,6 +0,0 @@
-function setDeleteAction() {
-    if(confirm("Kas olete kindel, et soovite valitud read kustutada?")) {
-    document.frmStudents.action = "DeleteMultiple.php";
-    document.frmStudents.submit();
-    }
-}
